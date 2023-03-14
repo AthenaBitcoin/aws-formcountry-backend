546d8f034a3b0c90be21bc3ecfde3dd75a320224
schwartrer <brian@athenabitcoin.com>
2023-03-14 16:12:33 -0500

    try using the logger

--- lambda/KycIdentityPy3/index.py
+++ lambda/KycIdentityPy3/index.py
@@ -4,6 +4,7 @@ from aws_lambda_powertools.utilities.typing import LambdaContext
 from aws_lambda_powertools.utilities.parser.models import APIGatewayProxyEventV2Model
 from typing import List, Optional
 
+from aws_lambda_powertools import Logger
 
 from aws_lambda_powertools.utilities.data_classes import event_source, APIGatewayProxyEventV2
 import base64
@@ -11,7 +12,7 @@ import os
 from http import HTTPStatus
 from typing import Any
 from aws_lambda_powertools.utilities.parameters.ssm import get_parameters_by_name
-
+from aws_lambda_powertools.logging import correlation_paths
 
 class UserIdentityLookup(BaseModel):
     user: str
@@ -27,7 +28,7 @@ class UserIdentityLookupGWModel(APIGatewayProxyEventV2Model):
 
 
 #import requests
-
+logger = Logger()
 import urllib3
 import json
 def get_jumio_token():
@@ -46,7 +47,7 @@ def get_jumio_token():
         "Content-Type": 'application/json',
         "Authorization": 'Basic ' + base64.b64encode(values['JUMIO_USER'] + ':' + values['JUMIO_TOKEN']).encode(),
         "User - Agent": 'AthenaMexOnboarding',
-        "Access - Control - Allow - Origin": '*'
+        "Access-Control-Allow-Origin": '*'
     }
     url = 'https://netverify.com/api/v4/initiate'
     #response = requests.post(url, headers=headers)
@@ -62,13 +63,24 @@ def get_jumio_token():
     print(response.text)
 
 
-@event_source(data_class=APIGatewayProxyEventV2)
-def handler(event: APIGatewayProxyEventV2, context):
+#@event_source(data_class=APIGatewayProxyEventV2)
+@logger.inject_lambda_context(correlation_id_path=correlation_paths.API_GATEWAY_REST, log_event=True)
+def handler(event: dict, context : LambdaContext):
     print(f"printing {event}")
+
+    logger.debug(f"Logging: CorrelationId = {logger.get_correlation_id()}")
     get_jumio_token()
     #
 
+    request = APIGatewayProxyEventV2(event)
+
     return {
         "status_code": HTTPStatus.OK,
-        "body": {}
+        "body": {},
+        "headers": {
+            #https://docs.aws.amazon.com/apigateway/latest/developerguide/how-to-cors.html
+            "Access-Control-Allow-Origin": '*',
+            "Access-Control-Allow-Headers": '*',
+            'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
+        }
     }

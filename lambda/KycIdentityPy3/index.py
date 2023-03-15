d6f2e8b8b8200f15ce8d184a3f6806590de816e7
schwartrer <brian@athenabitcoin.com>
2023-03-14 21:19:08 -0500

    how is this?

--- lambda/KycIdentityPy3/index.py
+++ lambda/KycIdentityPy3/index.py
@@ -31,58 +31,83 @@ class UserIdentityLookupGWModel(APIGatewayProxyEventV2Model):
 logger = Logger()
 import urllib3
 import json
-def get_jumio_token():
-    values: dict[str, Any] = get_parameters_by_name(parameters={
-        os.environ["JUMIO_TOKEN"]: {},
-        os.environ["JUMIO_USER"]: {}
-    }, raise_on_error=False)
-    errors: list[str] = values.get("_errors", [])
 
-    if len(errors):
-        print(errors)
-        raise RuntimeError()
-    logger.info(f"Values: {values}")
+
+def get_jumio_token(callback_host, locale, tokenLifeTime=86400):
+    values: dict[str, Any] = get_parameters_by_name(
+        parameters={
+            os.environ["JUMIO_TOKEN"]: {},
+            os.environ["JUMIO_USER"]: {}
+        },
+        decrypt=True,
+        raise_on_error=True)
+
+    #  https://github.com/Jumio/implementation-guides/blob/master/netverify/netverify-web-v4.md
+    auth_str = ':'.join([
+                      values[os.environ["JUMIO_USER"]],
+                      values[os.environ["JUMIO_TOKEN"]]])
     headers = {
         "Accept": 'application/json',
         "Content-Type": 'application/json',
-        "Authorization": 'Basic ' + base64.b64encode(
-            values[os.environ["JUMIO_USER"]] + ':' +
-            values[os.environ["JUMIO_TOKEN"]]).encode(),
-        "User - Agent": 'AthenaMexOnboarding',
-        "Access-Control-Allow-Origin": '*'
+        "Authorization": ' '.join([
+            'Basic',
+            base64.b64encode(auth_str.encode()).decode()]),
+        "User-Agent": 'AthenaBitcoin MexOnboarding/v1.0'
     }
     url = 'https://netverify.com/api/v4/initiate'
-    #response = requests.post(url, headers=headers)
 
+    body = {
+        "customerInternalReference": 'abc',
+        "userReference": 'def',
+        "reportingCriteria": None,
+        "successUrl": f"https://{callback_host}/callbacks/jumio/success",
+        "errorUrl": f"https://{callback_host}/callbacks/jumio/error",
+        "callbackUrl": f"https://{callback_host}/callbacks/jumio/callback",
+        "workflowId": 100,
+        "locale": locale,
+        "tokenLifetimeInMinutes": tokenLifeTime
+               }
     http = urllib3.PoolManager()
 
-    response = http.request('POST',
-                        url,
-                        body = {},# json.dumps(some_data_structure),
-                        headers = headers,
-                        retries = False)
+    response = http.request('POST', url,
+                        body=json.dumps(body).encode(),
+                        headers=headers,
+                        retries=False)
 
-    print(response.text)
+    if response.status != HTTPStatus.OK:
+        logger.error(f"JUMIO={response.status}!  {response.data.decode('utf-8')}")
+        raise RuntimeError("JUMIO!!!!")
+
+    result = json.loads(response.data.decode('utf-8'))
+    logger.debug(f"result: {result}")
+    return result
 
 
 #@event_source(data_class=APIGatewayProxyEventV2)
-@logger.inject_lambda_context(correlation_id_path=correlation_paths.API_GATEWAY_REST, log_event=True)
-def handler(event: dict, context : LambdaContext):
-    print(f"printing {event}")
+@logger.inject_lambda_context(correlation_id_path=correlation_paths.API_GATEWAY_REST,
+                              log_event=True)
+def handler(event: dict, context: LambdaContext):
 
     logger.debug(f"Logging: CorrelationId = {logger.get_correlation_id()}")
-    get_jumio_token()
-    #
 
     request = APIGatewayProxyEventV2(event)
 
+    logger.debug(f"Object: {request}")
+
+    r = get_jumio_token(host=event["headers"].get("Host"),
+                        # https://regex101.com/library/sYfs0V?orderBy=LEAST_POINTS&page=442&search=
+                        locale="es-MX")# get from accept language)
+    # keys = set(['timestamp', 'transactionReference', 'redirectUrl'}
+
     return {
-        "status_code": HTTPStatus.OK,
-        "body": {},
+        "statusCode": HTTPStatus.OK,
+        "body": {
+            "redirectUrl": r['redirectUrl']
+        },
         "headers": {
             #https://docs.aws.amazon.com/apigateway/latest/developerguide/how-to-cors.html
-            "Access-Control-Allow-Origin": '*',
-            "Access-Control-Allow-Headers": '*',
-            'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
+            #"Access-Control-Allow-Origin": '*',
+            #"Access-Control-Allow-Headers": '*',
+            #'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
         }
     }
